27863636b86d1b9812f146443ceee84450b17cd6
Omry Mendelovich <omry155@gmail.com>
2022-09-13 16:50:40 +0300

    add pipeline package

---
+++ pipeline/examples/csv/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"context"
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"github.com/playground/pipeline"
+	"io"
+	"os"
+	"strconv"
+)
+
+func main() {
+	source := fileReader{fileName: "source.csv"}
+	proccesor := func(ctx context.Context, in []pipeline.Item) []pipeline.Item {
+		res := make([]pipeline.Item, 0, len(in))
+		for i, p := range in {
+			tmp := p.Data.(item)
+			tmp.Price += i
+			p.Data = tmp
+			res = append(res, p)
+		}
+		return res
+	}
+	dest := fileWriter{filename: "output.csv"}
+	pipe := pipeline.CreatePipeline(pipeline.Config{Concurrency: 2, ChunkSize: 1}, &source, proccesor, &dest)
+	fmt.Println(pipe.Run(context.Background()))
+}
+
+type item struct {
+	ID, Desc      string
+	Amount, Price int
+}
+
+type fileReader struct {
+	fileName string
+}
+
+func (f *fileReader) List(ctx context.Context, offset, chunkSize int, sinkFn func(context.Context, []pipeline.Item) error, close func()) {
+	defer close()
+	// for the example, ignoring offset and chunk size
+	reader, err := os.Open(f.fileName)
+	if err != nil {
+		panic(err)
+	}
+	csvReader := csv.NewReader(reader)
+	i := 0
+	for {
+		line, err := csvReader.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else if errors.Is(err, csv.ErrFieldCount) {
+				fmt.Println(err)
+				continue
+			} else {
+				panic(err)
+			}
+		}
+		if i == 0 {
+			i++ // skip headers
+			continue
+		}
+		tmpItem := lineToItem(line)
+		items := []pipeline.Item{{
+			Data:   tmpItem,
+			Key:    tmpItem.ID,
+			Offset: i,
+		}}
+		err = sinkFn(ctx, items)
+		if err != nil {
+			panic(err)
+		}
+		i++
+	}
+}
+
+func lineToItem(line []string) item {
+	amount, _ := strconv.Atoi(line[2])
+	price, _ := strconv.Atoi(line[3])
+	return item{
+		ID:     line[0],
+		Desc:   line[1],
+		Amount: amount,
+		Price:  price,
+	}
+}
+func itemToLine(i item) []string {
+	return []string{i.ID, i.Desc, strconv.Itoa(i.Amount), strconv.Itoa(i.Price)}
+}
+
+type fileWriter struct {
+	filename string
+}
+
+func (f *fileWriter) Send(ctx context.Context, data chan []pipeline.Item) {
+	file, err := os.Create(f.filename)
+	if err != nil {
+		panic(err)
+	}
+
+	writer := csv.NewWriter(file)
+	err = writer.Write([]string{"id", "description", "amount", "price"})
+	if err != nil {
+		panic(err)
+	}
+	defer writer.Flush()
+
+	for items := range data {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			for _, p := range items {
+				err = writer.Write(itemToLine(p.Data.(item)))
+				if err != nil {
+					panic(err)
+				}
+
+			}
+		}
+
+	}
+}
---
+++ pipeline/pipeliner.go
@@ -0,0 +1,125 @@
+package pipeline
+
+import (
+	"context"
+	"sync"
+)
+
+type Logger interface {
+	Debug(args ...interface{})
+	Info(args ...interface{})
+	Warn(args ...interface{})
+	Error(args ...interface{})
+	Panic(args ...interface{})
+	Fatal(args ...interface{})
+}
+
+type Item struct {
+	Data   any
+	Key    string
+	Offset int
+}
+
+type Source interface {
+	List(ctx context.Context, offset, chunkSize int, sinkFn func(context.Context, []Item) error, close func())
+}
+
+type Processor func(ctx context.Context, items []Item) []Item
+
+type Destination interface {
+	Send(ctx context.Context, data chan []Item)
+}
+
+type Config struct {
+	Concurrency int
+	ChunkSize   int
+}
+
+type Pipeliner struct {
+	config      Config
+	source      Source
+	processor   Processor // serial processing in order
+	destination Destination
+}
+
+func Initialise(config Config, source Source, processor Processor, destination Destination) Pipeliner {
+
+	return Pipeliner{
+		config:      config,
+		source:      source,
+		processor:   processor,
+		destination: destination,
+	}
+}
+
+func stopIfErr(ctx context.Context, errs chan error, cancel context.CancelFunc, logger Logger) func(err error) bool {
+	return func(err error) bool {
+		if err == nil {
+			return false
+		}
+
+		if logger != nil {
+			logger.Error("pipeline error: ", err)
+		}
+
+		select {
+		case <-ctx.Done():
+			return false
+		case errs <- err:
+			cancel()
+		}
+
+		return true
+	}
+}
+
+func (p *Pipeliner) Run(ctx context.Context) error {
+	listChan := make(chan []Item)
+	sinkFn := func(ctx context.Context, items []Item) error {
+		listChan <- items
+		return nil
+	}
+	closeFn := func() {
+		close(listChan)
+	}
+	go p.source.List(ctx, 0, p.config.ChunkSize, sinkFn, closeFn)
+
+	processedChan := parallel(ctx, p.config.Concurrency, listChan, p.processor)
+	p.destination.Send(ctx, processedChan)
+
+	return nil
+}
+
+func parallel(ctx context.Context, numberOfPipes int, in chan []Item, fn func(ctx context.Context, items []Item) []Item) chan []Item {
+	processedChan := make(chan []Item)
+	go func() {
+		wg := &sync.WaitGroup{}
+		wg.Add(numberOfPipes)
+
+		for i := 0; i < numberOfPipes; i++ {
+			go func(index int) {
+				defer wg.Done()
+
+				for {
+					select {
+					case <-ctx.Done():
+						return
+					case v, ok := <-in:
+						if !ok {
+							return // channel was closed
+						}
+
+						processed := fn(ctx, v)
+						if len(processed) > 0 {
+							processedChan <- processed
+						}
+					}
+				}
+			}(i)
+		}
+
+		wg.Wait()
+
+	}()
+	return processedChan
+}
